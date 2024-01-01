1ad53274a6b2313842a1162a49b201abe2a14f07
NellDora <dorashin9471@gmail.com>
2024-01-01 23:37:08 +0900

    LV1 한문제

--- src/programers_level1/LV1CodingTest13.java
+++ src/programers_level1/LV1CodingTest13.java
@@ -1,7 +1,6 @@
 package programers_level1;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LV1CodingTest13 {
@@ -12,19 +11,34 @@ public class LV1CodingTest13 {
      */
     public static void main(String[] args){
         Solution13 solution13 = new Solution13();
+        int[] test1 = {1,2,3,4,6,7,8,0};
+        System.out.println(solution13.solution(test1));
 
-
+        int[] test2 = {5,8,4,0,6,7,9};
+        System.out.println(solution13.solution(test2));
 
     }
 }
 //이게 맞나?
 class Solution13 {
     public int solution(int[] numbers) {
-        int answer = -1;
+        int answer = 0;
         //일단 0부터 9까지의 값은 전부 꺼내놔야하는듯
-        int[] tmp = {0,1,2,3,4,5,6,7,8,9};
+        List<Integer> tmp = List.of(0,1,2,3,4,5,6,7,8,9);
+        List<Integer> numList = new ArrayList<>(tmp);
+
+        List<Integer> inputNum = new ArrayList<>();
+        for(int num : numbers){
+            inputNum.add(num);
+        }
+        numList.removeAll(inputNum);
+
+        for(int i : numList){
+            answer += i;
+        }
 
         //ArrayList에 넣어서 stream으로 매칭시켜서 빼야할 듯?
+
         return answer;
     }
 }

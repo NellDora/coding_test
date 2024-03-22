8b183c4e0739d0bd3d41f63d33a4676fcabd01ea
NellDora <dorashin9471@gmail.com>
2024-03-22 23:37:12 +0900

    LV0 한문제 , LV1 한 문제 풀이

---
+++ src/programers_level0/LV0CodingTest30.java
@@ -0,0 +1,32 @@
+package programers_level0;
+
+public class LV0CodingTest30 {
+    /*
+    문자열 곱하기
+문제 설명
+문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+제한사항
+1 ≤ my_string의 길이 ≤ 100
+my_string은 영소문자로만 이루어져 있습니다.
+1 ≤ k ≤ 100
+입출력 예
+my_string	k	result
+"string"	3	"stringstringstring"
+"love"	10	"lovelovelovelovelovelovelovelovelovelove"
+     */
+
+    public static void main (String[] args){
+        LV0Solution30 solution30 = new LV0Solution30();
+        System.out.println(solution30.solution("String",3));
+        System.out.println(solution30.solution("love",10));
+    }
+}
+
+class LV0Solution30{
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for(int i=1; i<=k; i++) answer+=my_string;
+        return answer;
+    }
+}
---
+++ src/programers_level0/LV0CodingTest31.java
@@ -0,0 +1,7 @@
+package programers_level0;
+
+import java.util.Arrays;
+
+public class LV0CodingTest31 {
+}
+

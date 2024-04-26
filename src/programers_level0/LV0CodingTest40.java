22fb41fa43477cc6c75a33ef96366c6f53776ef9
NellDora <dorashin9471@gmail.com>
2024-04-26 23:45:18 +0900

    LV0 한 문제 풀이

---
+++ src/programers_level0/LV0CodingTest40.java
@@ -0,0 +1,52 @@
+package programers_level0;
+
+public class LV0CodingTest40 {
+    /*
+    길이에 따른 연산
+문제 설명
+정수가 담긴 리스트 num_list가 주어질 때, 리스트의 길이가 11 이상이면 리스트에 있는 모든 원소의 합을 10 이하이면
+모든 원소의 곱을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+2 ≤ num_list의 길이 ≤ 20
+1 ≤ num_list의 원소 ≤ 9
+num_list의 원소를 모두 곱했을 때 2,147,483,647를 넘는 입력은 주어지지 않습니다.
+입출력 예
+num_list	result
+[3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1]	51
+[2, 3, 4, 5]	120
+입출력 예 설명
+입출력 예 #1
+
+리스트의 길이가 13이므로 모든 원소의 합인 51을 return합니다.
+입출력 예 #2
+
+리스트의 길이가 4이므로 모든 원소의 곱인 120을 return합니다.
+     */
+
+    public static void main( String[] args){
+        LV0Solution40 solution40 = new LV0Solution40();
+        System.out.println(solution40.solution(new int[]{3,4,5,2,5,4,6,7,3,7,2,2,1}));
+        System.out.println(solution40.solution(new int[]{2,3,4,5}));
+
+    }
+}
+
+class LV0Solution40{
+    public int solution(int[] num_list) {
+        int answer = 0;
+        if(num_list.length>=11){
+            for(int i : num_list){
+                answer += i;
+            }
+        }else{
+            for(int i: num_list){
+                if(answer==0){
+                    answer=1;
+                }
+                answer *=i;
+            }
+        }
+        return answer;
+    }
+}

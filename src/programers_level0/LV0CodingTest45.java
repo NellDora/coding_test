d6da7bd345b6a0e4dba83af97b54d56bab054eb7
NellDora <dorashin9471@gmail.com>
2024-07-01 23:25:40 +0900

    LV0 두 문제 풀이

---
+++ src/programers_level0/LV0CodingTest45.java
@@ -0,0 +1,52 @@
+package programers_level0;
+
+public class LV0CodingTest45 {
+    /*
+    각도기
+제출 내역
+문제 설명
+각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다.
+각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+
+예각 : 0 < angle < 90
+직각 : angle = 90
+둔각 : 90 < angle < 180
+평각 : angle = 180
+제한사항
+0 < angle ≤ 180
+angle은 정수입니다.
+입출력 예
+angle	result
+70	1
+91	3
+180	4
+입출력 예 설명
+입출력 예 #1
+
+angle이 70이므로 예각입니다. 따라서 1을 return합니다.
+     */
+    public static void main(String[] args){
+        LV0Solution45 solution45 = new LV0Solution45();
+        System.out.println(solution45.solution(70));
+        System.out.println(solution45.solution(91));
+        System.out.println(solution45.solution(180));
+    }
+}
+
+class LV0Solution45{
+    public int solution(int angle) {
+        int answer = 0;
+
+        if(angle<90){
+            answer=1;
+        }else if(angle==90){
+            answer =2;
+        }else if(angle<180){
+            answer = 3;
+        }else if(angle ==180){
+            answer=4;
+        }
+        return answer;
+    }
+
+}
